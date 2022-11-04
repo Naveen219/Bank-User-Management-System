668a80aa0be71e978a3d4877eae2c7f5a0a5e200
bachuruchitha <ruchitha.bachu9@gmail.com>
2022-11-04 19:19:07 +0530

    added registration page

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./Components/Login";
 import Menu from './Components/Menu';
 import Register from './Components/Register';
+import Header from './Components/Header';
 function App() {
   return (
     <>
     <Router>
-        
         <Routes>
-          <Route path="/login" element={<Login />}></Route>
+          <Route path="/" element={<Login />}></Route>
           <Route path='/register' element={<Register/>}></Route>
           <Route path='/menu' element={<Menu/>}></Route>
         </Routes>
