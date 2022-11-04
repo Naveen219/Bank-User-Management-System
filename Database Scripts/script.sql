eb46cd71c73a5ee6a75eebd8b6c05b6e615d76c0
Naveen Kumar <naveennnavy219@gmail.com>
2022-11-04 14:26:28 +0530

    updated script.sql

--- Database Scripts/script.sql
+++ Database Scripts/script.sql
@@ -1,26 +1,26 @@
 CREATE DATABASE IF NOT EXISTS gbuma;
 USE gbuma;
 
-CREATE TABLE IF NOT EXISTS `gbuma`.`customer_master` (
-  `customer_number` VARCHAR(6) NOT NULL,
-  `firstname` VARCHAR(30) NULL,
-  `middlename` VARCHAR(30) NULL,
-  `lastname` VARCHAR(30) NULL,
-  `customer_city` VARCHAR(30) NULL,
-  `customer_contact_no` VARCHAR(10) NULL,
-  `occupation` VARCHAR(20) NULL,
-  `customer_date_of_birth` DATE NULL,
-  PRIMARY KEY (`customer_number`));
+CREATE TABLE IF NOT EXISTS `customer_master` (
+  `customer_number` varchar(6) NOT NULL,
+  `firstname` varchar(30) DEFAULT NULL,
+  `middlename` varchar(30) DEFAULT NULL,
+  `lastname` varchar(30) DEFAULT NULL,
+  `customer_city` varchar(15) DEFAULT NULL,
+  `customer_contact_no` varchar(10) DEFAULT NULL,
+  `occupation` varchar(20) DEFAULT NULL,
+  `customer_date_of_birth` date DEFAULT NULL,
+  PRIMARY KEY (`customer_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
   
-  
-CREATE TABLE IF NOT EXISTS `gbuma`.`branch_master` (
-  `branch_id` VARCHAR(6) NOT NULL,
-  `branch_name` VARCHAR(30) NULL,
-  `branch_city` VARCHAR(30) NULL,
-  PRIMARY KEY (`branch_id`));
+CREATE TABLE IF NOT EXISTS `branch_master` (
+  `branch_id` varchar(6) NOT NULL,
+  `branch_name` varchar(30) DEFAULT NULL,
+  `branch_city` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`branch_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
-CREATE TABLE `loan_details` (
+CREATE TABLE IF NOT EXISTS `loan_details` (
   `customer_number` varchar(6) DEFAULT NULL,
   `branch_id` varchar(6) DEFAULT NULL,
   `loan_amount` int DEFAULT NULL,
@@ -28,4 +28,33 @@ CREATE TABLE `loan_details` (
   KEY `loan_branch_key_idx` (`branch_id`),
   CONSTRAINT `loan_branch_key` FOREIGN KEY (`branch_id`) REFERENCES `branch_master` (`branch_id`),
   CONSTRAINT `loan_customer_key` FOREIGN KEY (`customer_number`) REFERENCES `customer_master` (`customer_number`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS `account_master` (
+  `account_number` varchar(6) NOT NULL,
+  `customer_number` varchar(6) DEFAULT NULL,
+  `branch_id` varchar(6) DEFAULT NULL,
+  `opening_balance` int DEFAULT NULL,
+  `account_opening_date` date DEFAULT NULL,
+  `account_type` varchar(10) DEFAULT NULL,
+  `account_status` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`account_number`),
+  KEY `account_master_customer_master_key_idx` (`customer_number`),
+  KEY `account_master_branch_master_Fkey_idx` (`branch_id`),
+  CONSTRAINT `account_master_branch_master_Fkey` FOREIGN KEY (`branch_id`) REFERENCES `branch_master` (`branch_id`),
+  CONSTRAINT `account_master_customer_master_Fkey` FOREIGN KEY (`customer_number`) REFERENCES `customer_master` (`customer_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+CREATE TABLE IF NOT EXISTS `transaction_details` (
+  `transaction_number` varchar(6) NOT NULL,
+  `account_number` varchar(6) DEFAULT NULL,
+  `date_of_transaction` date DEFAULT NULL,
+  `medium_of_transaction` varchar(20) DEFAULT NULL,
+  `transaction_type` varchar(20) DEFAULT NULL,
+  `transaction_amount` int DEFAULT NULL,
+  PRIMARY KEY (`transaction_number`),
+  KEY `transaction_details_account_number_Fkey_idx` (`account_number`),
+  CONSTRAINT `transaction_details_account_number_Fkey` FOREIGN KEY (`account_number`) REFERENCES `account_master` (`account_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
