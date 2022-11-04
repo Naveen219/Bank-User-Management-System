dd3808bff142532e4fbb3a9dffe32915aafdac71
bachuruchitha <ruchitha.bachu9@gmail.com>
2022-11-04 15:09:41 +0530

    added menu

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Container, Button } from "reactstrap";
 
 const Menu = () => {
   return (
-    <div>
-      Menu Page
-    </div>
-  )
-}
+    <Container>
+      <Button color="primary" size="md" className="m-5">
+        Apply for Loan
+      </Button>
+      <Button color="secondary" size="md" className="m-5">
+        Transactions
+      </Button>
+      <Button color="warning" size="md" className="m-5">
+        View Statement
+      </Button>
+    </Container>
+  );
+};
 
-export default Menu
+export default Menu;
